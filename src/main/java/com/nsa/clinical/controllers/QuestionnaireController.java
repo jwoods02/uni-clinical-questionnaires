b11a0985f8a58158c3226aaddc7fdfb48fbf77b7
James Woods <woodsj13@cardiff.ac.uk>
2017-11-23 14:09:29 +0000

    Removed option create from questionnaire controller

--- src/main/java/com/nsa/clinical/controllers/QuestionnaireController.java
+++ src/main/java/com/nsa/clinical/controllers/QuestionnaireController.java
@@ -2,7 +2,6 @@ package com.nsa.clinical.controllers;
 
 import com.nsa.clinical.entities.Questionnaire;
 import com.nsa.clinical.forms.GetQuestionnaireForm;
-import com.nsa.clinical.forms.NewOptionForm;
 import com.nsa.clinical.forms.NewQuestionnaireForm;
 import com.nsa.clinical.services.QuestionnaireService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,26 +23,22 @@ public class QuestionnaireController {
     private QuestionnaireService questionnaireService;
 
     @Autowired
-    public QuestionnaireController(QuestionnaireService questionnaireService){
+    public QuestionnaireController(QuestionnaireService questionnaireService) {
         this.questionnaireService = questionnaireService;
     }
 
-    @RequestMapping(path = "/questionnaire/create", method = RequestMethod.POST)
-    public void createQuestionnaire(NewQuestionnaireForm newQuestionnaireForm){
+    @RequestMapping(path = "/questionnaire", method = RequestMethod.POST)
+    public void createQuestionnaire(NewQuestionnaireForm newQuestionnaireForm) {
         questionnaireService.newQuestionnaire(newQuestionnaireForm.getDescription());
     }
 
-    @RequestMapping(path = "questionnaire/get", method = RequestMethod.GET)
-    public @ResponseBody Questionnaire getQuestionnaire(GetQuestionnaireForm getQuestionnaireForm, HttpServletRequest request, HttpServletResponse response){
+    @RequestMapping(path = "/questionnaire", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    Questionnaire getQuestionnaire(GetQuestionnaireForm getQuestionnaireForm, HttpServletRequest request, HttpServletResponse response) {
         System.out.println(getQuestionnaireForm.getId());
         System.out.println(questionnaireService.retrieveQuestionnaire(getQuestionnaireForm.getId()).getQuestionnaireDescription());
         return questionnaireService.retrieveQuestionnaire(getQuestionnaireForm.getId());
     }
-
-    @RequestMapping(path = "/option/create", method = RequestMethod.POST)
-    public void createOption(NewOptionForm newOptionForm){
-        questionnaireService.newQuestionnaire(newOptionForm.getDescription());
-    }
-
 
 }
