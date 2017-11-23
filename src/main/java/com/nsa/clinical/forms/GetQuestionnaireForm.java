f70238d681b85d24c63cff24d54930e23d9cab42
c1673142 <ChandR@Cardiff.acuk>
2017-11-23 12:06:16 +0000

    Created getQuestionnaireForm.

---
+++ src/main/java/com/nsa/clinical/forms/GetQuestionnaireForm.java
@@ -0,0 +1,15 @@
+package com.nsa.clinical.forms;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by c1673142 on 23-Nov-17.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetQuestionnaireForm {
+    private Long QuestionnaireId;
+}
